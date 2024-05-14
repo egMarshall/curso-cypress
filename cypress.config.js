1051a02c7bde06a227ccc43d59d1ecde3e4ae449
Érico <ericogmarshall1@gmail.com>
2024-05-14 09:53:33 -0300

    [FEAT] Adiciona mochawesome ao projeto

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "nkcmjb", //Adiciona ao Cypress Cloud
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  video: true,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: true,
+    json: false,
+    timeStamp: 'mmddyyyy_HHMMss'
   },
 });
