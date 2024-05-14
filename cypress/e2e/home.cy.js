e51553109e351e8a9d400a6fe9108ca5c74e63e8
Érico <ericogmarshall1@gmail.com>
2024-05-14 10:32:59 -0300

    [FEAT] Adiciona verificação de imagens dos pets na home.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -8,8 +8,13 @@ describe('Página Home', () => {
     cy.contains('Falar com responsável').click({multiple: true});  
   });
 
+  it('Verifica imagens dos animais', () =>{
+    cy.get('.cards').should('be.visible');
+  });
+
   it("Retorna para a principal do AdoPet através do botão de home", () => {
     cy.get('.header__home').click();
     cy.url().should('eq', 'https://adopet-frontend-cypress.vercel.app/')
   });
+
 })
